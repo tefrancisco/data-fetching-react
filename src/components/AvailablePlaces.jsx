470e0d090288f83fc39e16840c004814790c424e
tefrancisco <dzfranciscodz@gmail.com>
2026-01-05 07:39:18 -0300

    Adding the loading text while fetching data

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,9 +3,11 @@ import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   // We need to use useEffect(), otherwise the state change will trigger an infinite loop
   useEffect(() => {
+    setIsLoading(true)
     // Method provided by the browser to get and send data with HTTP requests
     // This will send a GET request to this URL
     // The fetch() method returns a promise
@@ -15,13 +17,26 @@ export default function AvailablePlaces({ onSelectPlace }) {
       return response.json()
     }).then((resData) => {
       setAvailablePlaces(resData.places)
+      setIsLoading(false)
     })
+
+    // Same thing but using async and await:
+
+    // async function fetchPlaces() {
+    //   const response = await fetch('http://localhost:3000/places')
+    //   const resData = await response.json()
+    //   setAvailablePlaces(resData.places)
+    // }
+
+    // fetchPlaces()
   }, [])
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isLoading}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
